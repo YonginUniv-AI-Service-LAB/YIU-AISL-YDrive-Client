fa218348c39543fbbd5efc730da5eaf9ca8882c1
callikys <callikys@naver.com>
2024-03-06 17:46:44 +0900

    날짜 지정 형식 수정

--- lib/Screens/CarpoolScreen.dart
+++ lib/Screens/CarpoolScreen.dart
@@ -56,15 +56,15 @@ List<String> tiem1List = [
   '12'
 ];
 List<String> tiem2List = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
   '10',
   '11',
   '12',
@@ -1163,6 +1163,7 @@ class _CarpoolScreenState extends State<CarpoolScreen> {
                   ]);
             });
       } else {
+        print(response.statusCode);
         showDialog(
             context: context,
             builder: (BuildContext context) {
